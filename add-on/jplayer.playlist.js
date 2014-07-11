a20b6a687e8e160eaf85559247d005f2a59739f2
Dennis Lichtenthäler <dennis.lichtenthaeler@episode-iv.de>
2014-07-11 18:49:58 +0200

    Only allow download of MP3 files

--- add-on/jplayer.playlist.js
+++ add-on/jplayer.playlist.js
@@ -247,12 +247,9 @@
 				listItem += "<span class='" + this.options.playlistOptions.freeGroupClass + "'>(";
 				$.each(media, function(property,value) {
 					if($.jPlayer.prototype.format[property]) { // Check property is a media format.
-						if(first) {
-							first = false;
-						} else {
-							listItem += " | ";
+						if(property === 'mp3') {
+							listItem += "<a class='" + self.options.playlistOptions.freeItemClass + "' href='" + value + "' tabindex='1'>Download</a>";
 						}
-						listItem += "<a class='" + self.options.playlistOptions.freeItemClass + "' href='" + value + "' tabindex='1'>" + property + "</a>";
 					}
 				});
 				listItem += ")</span>";
